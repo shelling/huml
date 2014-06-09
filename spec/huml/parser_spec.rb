a29f9d0a5ec8e7fe9ec56dfb05fafcbadaae09ee
shelling <navyblueshellingford@gmail.com>
2014-06-09 16:31:23 +0800

    Wrap doctype with :multi

--- spec/huml/parser_spec.rb
+++ spec/huml/parser_spec.rb
@@ -4,6 +4,6 @@ describe HumlParser do
   subject { HumlParser.new }
 
   it "can recognize doctype" do
-    expect(subject.parse(" doctype 5 ").tokenize).to eq([:html, :doctype, "5"])
+    expect(subject.parse(" doctype 5 ").tokenize).to eq([:multi, [:html, :doctype, "5"]])
   end
 end
