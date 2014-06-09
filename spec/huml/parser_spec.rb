14e8baec96148852069a8ffb19282e335d46b8e9
shelling <navyblueshellingford@gmail.com>
2014-06-09 14:37:14 +0800

    the top rule can recognize doctype

--- spec/huml/parser_spec.rb
+++ spec/huml/parser_spec.rb
@@ -1,4 +1,9 @@
 require "spec_helper"
 
 describe HumlParser do
+  subject { HumlParser.new }
+
+  it "can recognize doctype" do
+    expect(subject.parse(" doctype 5 ").tokenize).to eq([:html, :doctype, "5"])
+  end
 end
