43f3d892f243c9d94778b4e218358fda0facb4dc
shelling <navyblueshellingford@gmail.com>
2014-06-13 00:33:30 +0800

    Space can be tokenized

--- spec/huml/parser_spec.rb
+++ spec/huml/parser_spec.rb
@@ -3,6 +3,15 @@ require "spec_helper"
 describe HumlParser do
   subject { HumlParser.new }
 
+  it "can recognize space" do
+    expect(subject.parse("\r", root: :space).tokenize).to eq([:newline])
+    expect(subject.parse("\n", root: :space).tokenize).to eq([:newline])
+    expect(subject.parse("  ", root: :space).tokenize).to eq(nil)
+    expect(subject.parse("\t", root: :space).tokenize).to eq(nil)
+    expect(subject.parse("  \t", root: :space).tokenize).to eq(nil)
+    expect(subject.parse("  \t\n\r", root: :space).tokenize).to eq([:newline])
+  end
+
   it "can recognize doctype" do
     expect(subject.parse(" doctype 5 ").tokenize).to eq([:multi, [:html, :doctype, "5"]])
   end
