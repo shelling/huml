865cc0600c8205872f63dd423fcf83ab56ef4e3c
shelling <navyblueshellingford@gmail.com>
2014-06-15 22:17:40 +0800

    Move tokenizers to be classes
    - Huml::Top for rule top
    - Huml::Doctype for rule doctype

--- lib/huml.rb
+++ lib/huml.rb
@@ -3,11 +3,24 @@ module Huml
   require "treetop"
   require "huml/parser"
 
+  class Top < Treetop::Runtime::SyntaxNode
+    def tokenize
+      [:multi].push(doctype.empty? ? [:multi] : doctype.tokenize)
+              .push(  block.empty? ? [:multi] : block.tokenize  )
+    end
+  end
+
   class Block < Treetop::Runtime::SyntaxNode
     def tokenize
       [:html, :tag, name.text_value.to_sym,
         [:multi],
         [:multi]]
+    end
+  end
+
+  class Doctype < Treetop::Runtime::SyntaxNode
+    def tokenize
+      [:html, :doctype, typestring.text_value]
     end
   end
 
