71cedae42e9354e91fba2eca1d6ceed854e9f521
shelling <navyblueshellingford@gmail.com>
2014-07-10 21:12:45 +0800

    Append filters AttributeMerger, AttributeRemover and ControlFlow

--- lib/huml/engine.rb
+++ lib/huml/engine.rb
@@ -3,7 +3,10 @@ require "temple"
 module Huml
   class Engine < ::Temple::Engine
     use Huml::Parser
+    use Temple::HTML::AttributeMerger
+    use Temple::HTML::AttributeRemover
     use Temple::HTML::Pretty
+    filter    :ControlFlow
     filter    :StaticMerger
     filter    :DynamicInliner
     generator :ArrayBuffer
